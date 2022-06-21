15b6992a28d2426ff968a639b89626e4602fbbfb
Luke Pickard <lukepc445@gmail.com>
2022-06-21 16:29:43 +1200

    backend:data added more data - Luke

--- data/departments.js
+++ data/departments.js
@@ -1,4 +1,4 @@
-import { itId, nursingId, opId, eitId, mitId } from "./ids.js";
+import { itId, nursingId, opId, eitId, mitId, designId, foodId, chefId, PTId, PEId, SportsScienceId, hairdressingId, beautyId } from "./ids.js";
 
 const departments = [
   {
@@ -9,6 +9,46 @@ const departments = [
   {
     _id: nursingId,
     name: "Nursing",
+    institutions:[opId, mitId],
+  },
+  {
+    _id: designId,
+    name: "Design",
+    institutions:[opId, mitId],
+  },
+  {
+    _id: foodId,
+    name: "Food",
+    institutions:[opId, mitId],
+  },
+  {
+    _id: chefId,
+    name: "Chef",
+    institutions:[opId, mitId],
+  },
+  {
+    _id: PTId,
+    name: "PT",
+    institutions:[opId, mitId],
+  },
+  {
+    _id: PEId,
+    name: "PE",
+    institutions:[opId, mitId],
+  },
+  {
+    _id: SportsScienceId,
+    name: "SportsScience",
+    institutions:[opId, mitId],
+  },
+  {
+    _id: hairdressingId,
+    name: "Hairdressing",
+    institutions:[opId, mitId],
+  },
+  {
+    _id: beautyId,
+    name: "beauty",
     institutions:[opId, mitId],
   },
 ];
--- data/ids.js
+++ data/ids.js
@@ -8,5 +8,14 @@ const mitId = new mongoose.mongo.ObjectId();
 // Departments
 const itId = new mongoose.mongo.ObjectId();
 const nursingId = new mongoose.mongo.ObjectId();
+const designId = new mongoose.mongo.ObjectId();
+const foodId = new mongoose.mongo.ObjectId();
+const chefId = new mongoose.mongo.ObjectId();
+const PTId = new mongoose.mongo.ObjectId();
+const PEId = new mongoose.mongo.ObjectId();
+const SportsScienceId = new mongoose.mongo.ObjectId();
+const hairdressingId = new mongoose.mongo.ObjectId();
+const beautyId = new mongoose.mongo.ObjectId();
 
-export { eitId, mitId, itId, nursingId, opId };
+
+export { eitId, mitId, itId, nursingId, opId, designId, foodId, chefId, PTId, PEId, SportsScienceId, hairdressingId, beautyId };
